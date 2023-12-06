e6ba60c4f77cf0da3956a4ed3305b95566d484e2
unknown <3054117680@qq.com>
2023-12-06 20:44:29 +0800

    Lab3:finish

--- Lab3/script/Lab3_node.py
+++ Lab3/script/Lab3_node.py
@@ -28,7 +28,7 @@ from kinematics import ik_transform
 lock = RLock()
 ik = ik_transform.ArmIK()
 
-target_colors = ['red', "blue", "yellow"]  # 3 turns
+target_colors = ['red', "blue", "green"]  # 3 turns
 cur_turn = 0
 steps = ['forward', 'grasp', 'turnAround', 'forward', 'place', 'turnAround']  # each has 6 steps
 cur_step = 0
@@ -60,7 +60,7 @@ flag = False  # whether it is ok for next step
 # cube tracking
 color_x_pid = PID.PID(P=0.06, I=0, D=0)
 color_y_pid = PID.PID(P=0.00003, I=0, D=0)
-apriltag_x_pid = PID.PID(P=0.06, I=0, D=0)  # pid初始化
+apriltag_x_pid = PID.PID(P=0.06, I=0, D=0)  # pidåå§å
 apriltag_y_pid = PID.PID(P=0.00003, I=0, D=0)
 result_sub = None
 heartbeat_timer = None
@@ -107,7 +107,8 @@ def move():
             # rospy.loginfo("cur_step: %d",cur_step)
             if steps[cur_step] == 'forward':
                 set_velocity.publish(85.0, 90.0, 0.0)
-                rospy.sleep(10.45)
+                rospy.sleep(10.45+0.3*cur_turn)
+                #rospy.sleep(0)
                 # stop
                 set_velocity.publish(0, 0, 0)
                 # next step
@@ -119,9 +120,12 @@ def move():
                     y_dis = 0.15
                     flag = False
                     visual_running('color', target_colors[cur_turn])
+                    rospy.loginfo("current target_color:%s",target_colors[cur_turn])
 
                 diff_x = abs(color_center_x - centreX)
                 diff_y = abs(color_center_y - centreY)
+
+                #rospy.loginfo("color block pos:x %d,y %d",color_center_x,color_center_y)
 
                 if diff_x < 10:
                     color_x_pid.SetPoint = color_center_x
@@ -129,7 +133,7 @@ def move():
                     color_x_pid.SetPoint = centreX
                 color_x_pid.update(color_center_x)
                 dx = color_x_pid.output
-                rospy.loginfo("dx: %f", dx)
+                #rospy.loginfo("dx: %f", dx)
                 x_dis += int(dx)
                 x_dis = 200 if x_dis < 200 else x_dis
                 x_dis = 800 if x_dis > 800 else x_dis
@@ -140,21 +144,21 @@ def move():
                     color_y_pid.SetPoint = centreY
                 color_y_pid.update(color_center_y)
                 dy = color_y_pid.output
-                rospy.loginfo("dy: %f", dy)
+                #rospy.loginfo("dy: %f", dy)
                 y_dis += dy
                 y_dis = 0.12 if y_dis < 0.12 else y_dis
                 y_dis = 0.28 if y_dis > 0.28 else y_dis
 
-                target = ik.setPitchRanges((0, round(y_dis, 4), 0.03), -180, -180, 0)
+                target = ik.setPitchRanges((0, round(y_dis, 4), 0.0), -180, -180, 0)
 
                 if target:
                     servo_data = target[1]
-                    rospy.loginfo("servo_data[3]: %d,servo_data[4]: %d,servo_data[5]: %d,x_dis: %d ",
-                                  servo_data['servo3'],
-                                  servo_data['servo4'], servo_data['servo5'], x_dis)
-                    bus_servo_control.set_servos(joints_pub, 20, ((3, servo_data['servo3']), (4, servo_data['servo4']),
+                    #rospy.loginfo("servo_data[3]: %d,servo_data[4]: %d,servo_data[5]: %d,x_dis: %d ",
+                                  #servo_data['servo3'],
+                                  #servo_data['servo4'], servo_data['servo5'], x_dis)
+                    bus_servo_control.set_servos(joints_pub, 200, ((3, servo_data['servo3']), (4, servo_data['servo4']),
                                                                   (5, servo_data['servo5']), (6, x_dis)))
-                    rospy.sleep(0.02)
+                    rospy.sleep(0.3)
                 if abs(dx) < 2 and abs(dy) < 0.003:
                     num += 1
                     if num == 10:
@@ -169,12 +173,11 @@ def move():
                     buzzer_pub.publish(0.1)
                     bus_servo_control.set_servos(joints_pub, 500, ((1, 20),))  # open claw
                     rospy.sleep(0.5)
-                    print(offset_y)
-                    target = ik.setPitchRanges((0, round(y_dis, 4), -0.08), -180, -180, 0)  # arm down
+                    target = ik.setPitchRanges((0-0.07, round(y_dis, 4), -0.08), -180, -180, 0)  # arm down
                     if target:
                         servo_data = target[1]
-                        rospy.loginfo("servo_data[3]: %d,servo_data[4]: %d,servo_d ata[5]: %d,x_dis: %d",
-                                      servo_data['servo3'], servo_data['servo4'], servo_data['servo5'], x_dis)
+                        #rospy.loginfo("servo_data[3]: %d,servo_data[4]: %d,servo_d ata[5]: %d,x_dis: %d",
+                                      #servo_data['servo3'], servo_data['servo4'], servo_data['servo5'], x_dis)
                         bus_servo_control.set_servos(joints_pub, 1000,
                                                      ((3, servo_data['servo3']), (4, servo_data['servo4']),
                                                       (5, servo_data['servo5']), (6, x_dis)))
@@ -182,7 +185,7 @@ def move():
                     bus_servo_control.set_servos(joints_pub, 500, ((1, 450),))  # close claw
                     rospy.sleep(0.8)
                     bus_servo_control.set_servos(joints_pub, 1500,
-                                                 ((1, 450), (2, 500), (3, 80), (4, 825), (5, 625), (6, 500)))  # 机械臂抬起来
+                                                 ((1, 450), (2, 500), (3, 80), (4, 825), (5, 625), (6, 500)))  # æºæ¢°èæ¬èµ·æ¥
                     rospy.sleep(1.5)
                     arm_move = False
                     # next step
@@ -190,152 +193,63 @@ def move():
             elif steps[cur_step] == 'turnAround':
                 # -0.55 * 2 = -0.1 = 90deg, thus -1.1 * 2 = -2.2 = 180 deg
                 set_velocity.publish(0.0, 90.0, -0.55)
-                rospy.sleep(4.0)
+                #rospy.sleep(0)
+                rospy.sleep(4.07)
                 # stop
                 set_velocity.publish(0.0, 0.0, 0.0)
                 rospy.sleep(1.0)
                 # next step / next turn
-                if cur_step < len(steps):
+                if cur_step < len(steps)-1:
                     cur_step += 1
                 else:
                     cur_turn += 1
+                    cur_step = 0
             elif steps[cur_step] == 'place':
                 if flag:
                     x_dis = 500
                     y_dis = 0.15
                     flag = False
-                    if cur_turn == 0:
-                        visual_running('apriltag', '')
-                    else:
-                        visual_running('color', target_colors[cur_turn - 1])  # last block's color
+                    #if cur_turn != 0:
+                        #visual_running('color', target_colors[cur_turn - 1])  # last block's color
 
-                if cur_turn == 0:
-                    diff_x = abs(apriltag_center_x - centreX)
-                    diff_y = abs(apriltag_center_y - centreY)
-
-                    if diff_x < 10:
-                        apriltag_x_pid.SetPoint = apriltag_center_x
-                    else:
-                        apriltag_x_pid.SetPoint = centreX
-                    apriltag_x_pid.update(apriltag_center_x)
-                    dx = apriltag_x_pid.output
-                    rospy.loginfo("dx: %f", dx)
-                    x_dis += int(dx)
-                    x_dis = 200 if x_dis < 200 else x_dis
-                    x_dis = 800 if x_dis > 800 else x_dis
-
-                    if diff_y < 10:
-                        apriltag_y_pid.SetPoint = apriltag_center_y
-                    else:
-                        apriltag_y_pid.SetPoint = centreY
-                    apriltag_y_pid.update(apriltag_center_y)
-                    dy = apriltag_y_pid.output
-                    rospy.loginfo("dy: %f", dy)
-                    y_dis += dy
-                    y_dis = 0.12 if y_dis < 0.12 else y_dis
-                    y_dis = 0.28 if y_dis > 0.28 else y_dis
-
-                    target = ik.setPitchRanges((0, round(y_dis, 4), 0.0), -180, -180, 0)
-                    if target:
-                        servo_data = target[1]
-                        rospy.loginfo("servo_data[3]: %d,servo_data[4]: %d,servo_data[5]: %d,x_dis: %d ",
-                                      servo_data['servo3'],
-                                      servo_data['servo4'], servo_data['servo5'], x_dis)
-                        bus_servo_control.set_servos(joints_pub, 20,
-                                                     ((3, servo_data['servo3']), (4, servo_data['servo4']),
-                                                      (5, servo_data['servo5']), (6, x_dis)))
-                        rospy.sleep(0.02)
-
+                #if cur_turn == 0:
+                target = ik.setPitchRanges((0, round(0.2, 4), 0), -180, -180, 0)
+                if target:
+                    servo_data = target[1]
+                    bus_servo_control.set_servos(joints_pub, 20,
+                                                 ((3, servo_data['servo3']), (4, servo_data['servo4']),
+                                                  (5, servo_data['servo5']), (6, x_dis)))
+                    rospy.sleep(0.02)
+                num += 1
+                rospy.loginfo("num:%d",num)
+                if num == 4:
+                    num = 0
+                    offset_y = Misc.map(target[2], -180, -150, -0.03, 0.03)
                     arm_move = True
-
-                    if arm_move:
-                        angle_pul = Misc.map(angle, 0, 80, 500, 800)
-                        bus_servo_control.set_servos(joints_pub, 500, ((2, angle_pul),))
-                        rospy.sleep(0.5)
-                        target = ik.setPitchRanges((0, round(y_dis + offset_y, 4), -0.08), -180, -180, 0)  # arm down
-                        if target:
-                            servo_data = target[1]
-                            bus_servo_control.set_servos(joints_pub, 1000,
-                                                         ((3, servo_data['servo3']), (4, servo_data['servo4']),
-                                                          (5, servo_data['servo5']), (6, x_dis)))
-                        rospy.sleep(1.0)
-                        buzzer_pub.publish(0.1)
-                        bus_servo_control.set_servos(joints_pub, 500, ((1, 120),))  # open claw
-                        rospy.sleep(0.5)
-
-                        # arm reset
-                        bus_servo_control.set_servos(joints_pub, 1500,
-                                                     ((1, 120), (2, 500), (3, 80), (4, 825), (5, 625),
-                                                      (6, 500)))  # arm up
-                        rospy.sleep(1.5)
-                else:
-                    diff_x = abs(color_center_x - centreX)
-                    diff_y = abs(color_center_y - centreY)
-
-                    if diff_x < 10:
-                        color_x_pid.SetPoint = color_center_x
-                    else:
-                        color_x_pid.SetPoint = centreX
-                    color_x_pid.update(color_center_x)
-                    dx = color_x_pid.output
-                    rospy.loginfo("dx: %f", dx)
-                    x_dis += int(dx)
-                    x_dis = 200 if x_dis < 200 else x_dis
-                    x_dis = 800 if x_dis > 800 else x_dis
-
-                    if diff_y < 10:
-                        color_y_pid.SetPoint = color_center_y
-                    else:
-                        color_y_pid.SetPoint = centreY
-                    color_y_pid.update(color_center_y)
-                    dy = color_y_pid.output
-                    rospy.loginfo("dy: %f", dy)
-                    y_dis += dy
-                    y_dis = 0.12 if y_dis < 0.12 else y_dis
-                    y_dis = 0.28 if y_dis > 0.28 else y_dis
-
-                    target = ik.setPitchRanges((0, round(y_dis, 4), 0.03), -180, -180, 0)
-
+                if arm_move:
+                    rospy.loginfo("ready to place april tag")
+                    angle_pul = Misc.map(angle, 0, 80, 500, 800)
+                    bus_servo_control.set_servos(joints_pub, 500, ((2, angle_pul),))
+                    rospy.sleep(0.5)
+                    target = ik.setPitchRanges((0, round(0.2, 4), -0.08+0.03*cur_turn), -180, -180, 0)  # arm down
                     if target:
                         servo_data = target[1]
-                        rospy.loginfo("servo_data[3]: %d,servo_data[4]: %d,servo_data[5]: %d,x_dis: %d ",
-                                      servo_data['servo3'],
-                                      servo_data['servo4'], servo_data['servo5'], x_dis)
-                        bus_servo_control.set_servos(joints_pub, 20,
+                        bus_servo_control.set_servos(joints_pub, 1000,
                                                      ((3, servo_data['servo3']), (4, servo_data['servo4']),
                                                       (5, servo_data['servo5']), (6, x_dis)))
-                        rospy.sleep(0.02)
-                    if abs(dx) < 2 and abs(dy) < 0.003:
-                        num += 1
-                        if num == 10:
-                            num = 0
-                            offset_y = Misc.map(target[2], -180, -150, -0.03, 0.03)
-                            arm_move = True  # ready to place
-                    else:
-                        num = 0
-
-                    if arm_move:
-                        buzzer_pub.publish(0.1)
-                        target = ik.setPitchRanges((0, round(y_dis, 4), -0.08), -180, -180, 0)  # arm down
-                        if target:
-                            servo_data = target[1]
-                            rospy.loginfo("servo_data[3]: %d,servo_data[4]: %d,servo_d ata[5]: %d,x_dis: %d",
-                                          servo_data['servo3'], servo_data['servo4'], servo_data['servo5'], x_dis)
-                            bus_servo_control.set_servos(joints_pub, 1000,
-                                                         ((3, servo_data['servo3']), (4, servo_data['servo4']),
-                                                          (5, servo_data['servo5']), (6, x_dis)))
-                        rospy.sleep(1.5)
-                        bus_servo_control.set_servos(joints_pub, 500, ((1, 20),))  # open claw
-                        rospy.sleep(0.5)
-                        bus_servo_control.set_servos(joints_pub, 500, ((1, 450),))  # close claw
-                        rospy.sleep(0.8)
-                        bus_servo_control.set_servos(joints_pub, 1500,
-                                                     ((1, 450), (2, 500), (3, 80), (4, 825), (5, 625),
-                                                      (6, 500)))  # arm up
-                        rospy.sleep(1.5)
-                        arm_move = False
+                    rospy.sleep(1.1)
+                    buzzer_pub.publish(0.1)
+                    bus_servo_control.set_servos(joints_pub, 500, ((1, 120),))  # open claw
+                    rospy.sleep(0.5)
+                    # arm reset
+                    bus_servo_control.set_servos(joints_pub, 1500,
+                                                 ((1, 120), (2, 500), (3, 80), (4, 825), (5, 625),
+                                                  (6, 500)))  # arm up
+                    rospy.sleep(1.5)
+                    arm_move = False
+                    cur_step += 1
                 # next step
-                cur_step += 1
+
     reset()
 
 
